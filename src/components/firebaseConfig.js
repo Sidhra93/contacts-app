50b211ef72c43696ff52fe210ecfc07563e74126
Sidhra Cader <clmsdc@mac14889.ccla.com.au>
2018-06-27 17:04:54 +1000

    contacts app

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: 'AIzaSyDhN_v5U58Vgw7FWIO-wmvzTo8D5n5tz4s',
+  authDomain: 'contact-app-f57d3.firebaseapp.com',
+  databaseURL: 'https://contact-app-f57d3.firebaseio.com',
+  projectId: 'contact-app-f57d3',
+  storageBucket: 'contact-app-f57d3.appspot.com',
+  messagingSenderId: '644561115106'
+}
